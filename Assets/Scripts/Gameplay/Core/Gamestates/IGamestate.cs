cc559a204420a88557b5f102cfbed49489874b92
gormed <hans.ferchland@gmx.de>
2014-03-25 22:29:16 +0100

    added general gamestate system

---
+++ Assets/Scripts/Gameplay/Core/Gamestates/IGamestate.cs
@@ -0,0 +1,21 @@
+﻿using Gameplay.Core.Type;
+
+namespace Gameplay.Core.Gamestates 
+{
+
+	public interface IGameState 
+	{
+
+		string GetName();
+
+		GamestateType GetGamestateType();
+
+		void Enter();
+
+		void Leave();
+
+		void Reset();
+
+	}
+
+}
