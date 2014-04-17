3fc6f604c917b1f5b15c7efe9977a177bd131ea0
Siftos <siftos@gmail.com>
2014-04-17 11:40:38 +0200

    added constant horizontal force and drag

--- Assets/Scripts/Gameplay/Core/Controls/GroundController.cs
+++ Assets/Scripts/Gameplay/Core/Controls/GroundController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class GroundController : MonoBehaviour, IInputListener
 {
 
-		// Use this for initialization
+// Use this for initialization
 		void Start ()
 		{
 				UnityEngine.Object.DontDestroyOnLoad (this);
@@ -14,25 +14,36 @@ public class GroundController : MonoBehaviour, IInputListener
 
 				LookRight = transform.rotation;
 				LookLeft = LookRight * Quaternion.Euler (0, 180, 0); 
-				
-				//Length of Raycast to check ground considering collider offset
+
+//Length of Raycast to check ground considering collider offset
 				RayLength = collider.bounds.size.y / 2 - Math.Abs (transform.position.y - collider.bounds.center.y);
 		}
-	
-		// Update is called once per frame
+
+// Update is called once per frame
 		void Update ()
 		{
-
 		}
 
 		void FixedUpdate ()
 		{		
-				//Jump
+				if (IsGrounded ()) {
+						rigidbody.AddForce (Vector3.right * MovementForce * MovementSpeed);
+						MovementForce *= 1.0f - Drag;
+
+				}
+
+				if (Mathf.Abs (rigidbody.velocity.x) > MaxSpeed) {
+						Velocity = rigidbody.velocity;
+						Velocity.x = Mathf.Sign (rigidbody.velocity.x) * MaxSpeed;
+						rigidbody.velocity = Velocity;
+				}
+
+//Jump
 				if (TriggeredJump) {
 						Jump ();
 				}
 
-				//Apply own Gravity
+//Apply own Gravity
 				rigidbody.AddForce (-Vector3.up * Gravity * rigidbody.mass);
 
 		}
@@ -44,17 +55,17 @@ public class GroundController : MonoBehaviour, IInputListener
 
 		public void OnButtonPressed (string button)
 		{
-	
+
 		}
 
 		public void OnButtonDown (string button)
 		{
-		
+
 		}
-	
+
 		public void OnAxis (string axisName, float axisValue)
 		{
-		
+
 				if (Math.Abs (axisValue) > AxisMax && !m_MaxAxis.ContainsKey (axisName)) {
 						m_MaxAxis.Add (axisName, axisValue);
 						if (axisName == InputStringMapping.GroundInputMapping.P1_NavigateHorizontal) {
@@ -69,9 +80,9 @@ public class GroundController : MonoBehaviour, IInputListener
 				}
 
 				float value;
-		
+
 				if ((Math.Abs (axisValue) < AxisMax && m_MaxAxis.TryGetValue (axisName, out value))) {
-			
+
 						if (AxisInUse.ContainsKey (InputStringMapping.GroundInputMapping.P1_L_Step) && AxisInUse.ContainsKey (InputStringMapping.GroundInputMapping.P1_R_Step)) {
 								if (Math.Abs (AxisInUse [InputStringMapping.GroundInputMapping.P1_L_Step] - AxisInUse [InputStringMapping.GroundInputMapping.P1_R_Step]) <= 0.05) {
 										TriggeredJump = true;
@@ -79,7 +90,7 @@ public class GroundController : MonoBehaviour, IInputListener
 										m_MaxAxis.Clear ();
 								}
 						}
-			
+
 						if (axisName == InputStringMapping.GroundInputMapping.P1_L_Step && m_MaxAxis.ContainsKey (InputStringMapping.GroundInputMapping.P1_NavigateHorizontal)) {
 								HAxis = m_MaxAxis [(InputStringMapping.GroundInputMapping.P1_NavigateHorizontal)];
 								TrigL = true;	
@@ -90,7 +101,7 @@ public class GroundController : MonoBehaviour, IInputListener
 										Move (false);
 								}
 						}
-			
+
 						if (axisName == InputStringMapping.GroundInputMapping.P1_R_Step && m_MaxAxis.ContainsKey (InputStringMapping.GroundInputMapping.P1_NavigateHorizontal)) {
 								HAxis = m_MaxAxis [(InputStringMapping.GroundInputMapping.P1_NavigateHorizontal)];
 								TrigR = true;	
@@ -100,42 +111,39 @@ public class GroundController : MonoBehaviour, IInputListener
 								} else {
 										Move (false);
 								}
-				
-				
-				
+
+
+
 						}
 						m_MaxAxis.Remove (axisName);
 				}
-			
 
-		
+
+
 		}
-	
+
 		public void OnMovement (string moveName, int x, int y)
 		{
-	
+
 		}
 
 		public void Move (bool dstep)
 		{		
-				HAxis *= MovementForce;
 				Flip (HAxis);
-				if (rigidbody.velocity.magnitude < MaxSpeed && IsGrounded () == true) {
-						if (dstep) {
-								//rigidbody.AddForce (Vector3.right * HAxis * MovementSpeed*2);
-								rigidbody.velocity = rigidbody.velocity + (Vector3.right * HAxis);
-						} else {
-								//rigidbody.AddForce (Vector3.right * HAxis * MovementSpeed);
-								rigidbody.velocity = rigidbody.velocity + (Vector3.right * HAxis * 0.5f);
-						}
+				if (dstep) {
+						MaxSpeed = MaxSpeed_2;
+						MovementForce += 0.5f * MovementSpeed * Math.Sign (HAxis);
+				} else {
+						MaxSpeed = MaxSpeed_1;
+						MovementForce += 0.5f * MovementSpeed * Math.Sign (HAxis);
 				}
 		}
 
 		public void Jump ()
 		{
 				if (IsGrounded ()) {					
-						TriggeredJump = false;
 						rigidbody.velocity = rigidbody.velocity + (Vector3.up * JumpSpeed);
+						TriggeredJump = false;
 				}
 		}
 
@@ -148,7 +156,7 @@ public class GroundController : MonoBehaviour, IInputListener
 						transform.rotation = LookLeft; 
 				}
 		}
-		
+
 		public bool IsGrounded ()
 		{
 				if (Physics.Raycast (transform.position, -transform.up, RayLength)) {
@@ -161,26 +169,27 @@ public class GroundController : MonoBehaviour, IInputListener
 
 		private Dictionary<string, float>		m_MaxAxis = new Dictionary<string, float> ();
 		private Dictionary<string, float>		AxisInUse = new Dictionary<string, float> ();
-	
-		////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
 		public InputManager						GlobalInput;
 		bool 									Grounded;
 		float 									RayLength;
 		float 									HAxis;
+		float 									MaxSpeed = 0;
+		float 									MovementForce;
+		Vector3 								Velocity;
 		bool									TriggeredJump = false;
 		bool    								TrigL = false;
 		bool 									TrigR = false;
 		Quaternion								LookRight;
 		Quaternion								LookLeft;
+		public float 							Drag = 0.3f;
 		public float							Gravity = 9.81f;
+		public float 							MaxSpeed_1 = 5;
+		public float 							MaxSpeed_2 = 10;
 		public float							MovementSpeed = 10;
-		public float							MovementForce = 5;
-		public float							MaxSpeed = 10;
 		public float							JumpSpeed = 10;
 		public float							AxisThreshold = 0;
 		public float							AxisMax = 0.9f;
 
-		// not implemented yet
-		float 									JumpTime;
-		float 									AirTime;
 }
