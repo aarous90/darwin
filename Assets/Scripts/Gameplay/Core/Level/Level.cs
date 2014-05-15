c9f5886bf10b24c75df3720b90c4c2efdf573930
Marcel Müller <marcel.mueller3101@gmail.com>
2014-05-15 21:50:48 +0200

    commit to get my stuff on my tower pc

--- Assets/Scripts/Gameplay/Core/Level/Level.cs
+++ Assets/Scripts/Gameplay/Core/Level/Level.cs
@@ -60,7 +60,7 @@ public class Level : MonoBehaviour
 	/// <param name="module">The Module to spawn in.</param>
 	void DoSpawn(SpawnModule module)
 	{
-		List<int> spawns = new List<int>(new int[] {0, 1, 2});
+		List<int> spawns = new List<int>(new int[] {0, 0, 0});
 
 		int maxSpawns = Mathf.Min(2, ControllerManager.Get().GetControllers().Count);
 
