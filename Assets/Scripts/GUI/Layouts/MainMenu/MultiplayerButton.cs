3119e4c2e8c51c870d91d7947f408023311ecc06
gormed <hans.ferchland@gmx.de>
2014-05-30 19:48:32 +0200

    * added template modules * ballon-barrel prefab added * some test ballons in IndustrialSector * some actions enhances (FallingAction now also has force) * scaled characters a little

--- Assets/Scripts/GUI/Layouts/MainMenu/MultiplayerButton.cs
+++ Assets/Scripts/GUI/Layouts/MainMenu/MultiplayerButton.cs
@@ -6,6 +6,13 @@ public class MultiplayerButton : Button
 	{
 		if (ControllerManager.Get().AvailabeJoystickCount >= 2)
 		{
+			SettingsManager.LevelSettings levelSettings = SettingsManager.Get().CurrentLevelSetings;
+			// use random chars in mp
+			levelSettings.PreferedLayer [0] = -1;
+			levelSettings.PreferedLayer [1] = -1;
+			
+			SettingsManager.Get().ApplyLevelSettings(levelSettings);
+
 			GamestateManager.Get().ChangeState(GamestateType.Multiplayer);
 		}
 	}
--- Assets/Scripts/Gameplay/Core/Level/Elements/Action/FallingAction.cs
+++ Assets/Scripts/Gameplay/Core/Level/Elements/Action/FallingAction.cs
@@ -22,6 +22,29 @@ public class FallingAction : AbstractAction
 	{
 		//GameObject go = GameObject.Find(name);
 		gameObject.rigidbody.useGravity = true;
+		if (Util.Length(Force) > 0)
+		{
+			Vector3 force;
+			if (UseRandomForce)
+			{
+				force = new Vector3((float) Util.Randomizer.NextDouble() * Force.x,
+				                    (float) Util.Randomizer.NextDouble() * Force.y,
+				                    (float) Util.Randomizer.NextDouble() * Force.z);
+			}
+			else
+			{
+				force = Force;
+			}
+//			gameObject.rigidbody.AddForce(
+//				force); 
+//			gameObject.rigidbody.AddTorque(
+//				force); 
+			gameObject.rigidbody.AddRelativeForce(
+				force); 
+			gameObject.rigidbody.AddRelativeTorque(
+				force); 
+				//gameObject.collider.ClosestPointOnBounds(gameObject.transform.position - (force.normalized * 100.0f)));
+		}
 		if (FadeTime > 0)
 		{
 			Destroy(gameObject, FadeTime);
@@ -31,5 +54,7 @@ public class FallingAction : AbstractAction
 	#endregion
 
 	public float FadeTime = 0;
+	public Vector3 Force = new Vector3();
+	public bool UseRandomForce;
 }
  
