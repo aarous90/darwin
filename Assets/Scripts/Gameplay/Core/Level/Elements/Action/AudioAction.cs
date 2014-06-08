77079edc22c2fff8f690ae1f06975a02f9df7609
Siftos <siftos@gmail.com>
2014-06-08 18:08:47 +0200

    *AudioAction *Added Sound to mine(urchin) prefab

---
+++ Assets/Scripts/Gameplay/Core/Level/Elements/Action/AudioAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AudioAction : AbstractAction
+{
+	protected override void DoAction(Component other)
+	{
+		if (ClipsToPlay.Length > 0)
+		{
+			for (int i = 0; i < ClipsToPlay.Length; i++)
+			{
+				Vector3 AudioOrigin = new Vector3(transform.position.x - other.transform.position.x, 0, 0);
+				AudioSource.PlayClipAtPoint(ClipsToPlay [i], AudioOrigin);
+			}
+		}
+	}
+
+	public AudioClip[] ClipsToPlay;
+}
+
